730259f4489634b39ba4f589793cbdd15128775b
Jovahnn Tucker <Jovahnn@yahoo.com>
2024-01-11 09:23:09 -0500

    Instafoller class created

---
+++ main.py
@@ -0,0 +1,25 @@
+from selenium import webdriver
+
+SIMILAR_ACCOUNT = "INSTAGRAM ACCOUNT YOU WANT TO BECOME"
+USERNAME = " YOUR INSTAGRAM USERNAME "
+PASSWORD = " YOUR PASSWORD "
+
+class InstaFollower:
+    def __init__(self):
+        chrome_options = webdriver.ChromeOptions()
+        chrome_options.add_experimental_option("detach", True)
+        self.driver = webdriver.Chrome(options=chrome_options)
+
+    def login(self):
+        pass
+
+    def find_followers(self):
+        pass
+
+    def follow(self):
+        pass
+
+bot = InstaFollower()
+bot.login()
+bot.find_followers()
+bot.follow()
